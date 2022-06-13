bcf4123ff8700fc98923ced14a01cec4705057d7
pjcwilliamson <pjcwilliamson@outlook.com>
2022-06-12 21:45:44 -0400

    End of video 4. Start with vid 5 next

--- app/src/main/java/org/williamsonministry/roomexample/NoteDatabase.java
+++ app/src/main/java/org/williamsonministry/roomexample/NoteDatabase.java
@@ -1,10 +1,13 @@
 package org.williamsonministry.roomexample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class}/*This is an array if I want more entities. Right now, an array.size()==1 */, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -13,13 +16,41 @@ public abstract class NoteDatabase extends RoomDatabase {
 
     public abstract NoteDao noteDao();
 
-    public static synchronized /*synchronized means only one thread can access at a time*/ NoteDatabase getInstance(Context context)    {
-        if (instance == null)   {
+    public static synchronized /*synchronized means only one thread can access at a time*/ NoteDatabase getInstance(Context context) {
+        if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
-            NoteDatabase.class, "note_database")
-            .fallbackToDestructiveMigration()/*deletes database if new version number to stop crashes*/
-            .build();
+                    NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()/*deletes database if new version number to stop crashes*/
+                    .addCallback(roomCallback) /*This callback the DB initiate with some initial data*/
+                    .build();
         }
         return instance;
+    }
+
+    // TODO: 6/12/2022 THIS VIDEO NEXT: https://www.youtube.com/watch?v=JLwW5HivZg4 
+
+    //This below is all to make it make a database when we first open the app.
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1", "Desc1", 1));
+            noteDao.insert(new Note("Title2", "Desc2", 2));
+            noteDao.insert(new Note("Title3", "Desc3", 3));
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/org/williamsonministry/roomexample/NoteRepository.java
@@ -0,0 +1,95 @@
+package org.williamsonministry.roomexample;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteRepository {
+    private NoteDao noteDao;
+    private LiveData<List<Note>> allNotes;
+
+    public NoteRepository(Application application) {
+        NoteDatabase database = NoteDatabase.getInstance(application);
+        noteDao = database.noteDao(); /*Normally we can't call an abstract method, but we can here because the builder in the database.getInstance uses fancy Room whatevers to make the noteDao() method a subclass or something*/
+        allNotes = noteDao.getAllNotes();
+    }
+
+    public void insert(Note note)   {
+        new InsertNoteAsyncTask(noteDao).execute(note); /*.execute comes from AsyncTask extension and passing note as required by first argument in extension below*/
+    }
+
+    public void update(Note note)   {
+        new UpdateNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void delete(Note note)   {
+        new DeleteNoteAsyncTask(noteDao).execute(note);
+    }
+
+    public void deleteAllNotes()    {
+        new DeleteAllNotesAsyncTask(noteDao).execute();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes; /*Room automatically executes LiveData functions on the database on a background thread, so this one is good. The rest we have to manually make background or else it'll crash cos Room doesn't allow foreground db operations0*/
+    }
+
+    private static /*must be static so it doesn't have a reference to the repository itself - that would cause a memory leak*/ class InsertNoteAsyncTask extends AsyncTask<Note, Void, Void>  /*pass, update, return*/  {
+        private NoteDao noteDao;
+
+        private InsertNoteAsyncTask (NoteDao noteDao)   {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.insert(notes[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        private UpdateNoteAsyncTask (NoteDao noteDao)   {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.update(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteNoteAsyncTask extends AsyncTask<Note, Void, Void> {
+        private NoteDao noteDao;
+
+        private DeleteNoteAsyncTask (NoteDao noteDao)   {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            noteDao.delete(notes[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllNotesAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private DeleteAllNotesAsyncTask (NoteDao noteDao)   {
+            this.noteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.deleteAllNotes();
+            return null;
+        }
+    }
+}
